69edc44bef008859666870219e609b01b46108af
KmInno <innocentmuwanda0@gmail.com>
2024-12-18 06:37:57 +0300

    ll

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ import { loadHTML } from './utils.mjs';
 
 // Load the header and footer on all pages
 document.addEventListener('DOMContentLoaded', () => {
-  loadHTML('/public/header.html', 'header');
-  loadHTML('/public/footer.html', 'footer');
+  loadHTML('../public/header.html', 'header');
+  loadHTML('../public/footer.html', 'footer');
 });
